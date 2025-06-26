2a5378211fc4bcce98290a9ef40b1f9c365c78cd
isaacwallace123 <goosewal@gmail.com>
2025-06-25 23:59:34 -0400

    Added a router module to mitigate web interactions

--- response/ResponseEntity.go
+++ response/ResponseEntity.go
@@ -1,20 +1,15 @@
 package response
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
 type ResponseEntity struct {
-	status  int
-	headers map[string]string
-	body    any
+	status int
+	header map[string]string
+	body   any
 }
 
-func Status(status int) *ResponseEntity {
+func Status(code int) *ResponseEntity {
 	return &ResponseEntity{
-		status:  status,
-		headers: make(map[string]string),
+		status: code,
+		header: make(map[string]string),
 	}
 }
 
@@ -24,17 +19,6 @@ func (r *ResponseEntity) Body(body any) *ResponseEntity {
 }
 
 func (r *ResponseEntity) Header(key, value string) *ResponseEntity {
-	r.headers[key] = value
+	r.header[key] = value
 	return r
-}
-
-func (r *ResponseEntity) Send(w http.ResponseWriter) {
-	for k, v := range r.headers {
-		w.Header().Set(k, v)
-	}
-	w.WriteHeader(r.status)
-	if r.body != nil {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(r.body)
-	}
 }
