f55ba269157ae020ea5ea014f9ae16710b524dc4
isaacwallace123 <goosewal@gmail.com>
2025-07-07 02:05:31 -0400

    type error bug fix

--- app/types/response_entity.go
+++ app/types/response_entity.go
@@ -1,6 +1,6 @@
 package types
 
-import resp "github.com/isaacwallace123/GoWeb/pkg/responseentity"
+import resp "github.com/isaacwallace123/GoWeb/pkg/ResponseEntity"
 
 // Re-export the type so users can reference types.ResponseEntity
 type ResponseEntity = resp.ResponseEntity
