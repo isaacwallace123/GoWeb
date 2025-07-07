4061e622f876b96ef3543989829354792d02420d
isaacwallace123 <goosewal@gmail.com>
2025-07-07 04:01:45 -0400

    Added HttpMethod.go as an enum for all HttpMethods, created a middleware builder for developers to use, added CORS middleware, updated logging middleware to utilize the builder, added a middleware type, updated router to run the middlewares before handling OPTIONS and such, updated controller type to use new middleware type

--- app/types/middleware.go
+++ app/types/middleware.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"github.com/isaacwallace123/GoUtils/logger"
 	"net/http"
 )
 
@@ -14,8 +15,13 @@ type MiddlewareContext struct {
 }
 
 // MiddlewareFunc represents a single middleware function.
-// It receives a MiddlewareContext and returns an error if any.
 type MiddlewareFunc func(ctx *MiddlewareContext) error
+
+// Middleware represents a middleware object with a Func method.
+// This allows middleware structs to be registered and called.
+type Middleware interface {
+	Func() MiddlewareFunc
+}
 
 // Next advances the middleware chain to the next function.
 // It returns any error produced by the next middleware.
@@ -27,8 +33,69 @@ func (ctx *MiddlewareContext) Next() error {
 	return nil // End of middleware chain
 }
 
-// PreMiddlewares holds globally registered middleware that runs before the handler.
-var PreMiddlewares []MiddlewareFunc
+// PreMiddlewares holds globally registered middleware objects.
+var PreMiddlewares []Middleware
 
-// PostMiddlewares holds globally registered middleware that runs after the handler.
-var PostMiddlewares []MiddlewareFunc
+// PostMiddlewares holds globally registered middleware objects.
+var PostMiddlewares []Middleware
+
+// --- Middleware Builder Pattern --- \\
+
+// MiddlewareBuilder is a reusable, typed middleware object with attached config and logic.
+type MiddlewareBuilder[T any] struct {
+	Config         *T
+	Handler        MiddlewareFunc
+	OnErrorHandler func(ctx *MiddlewareContext, err error)
+}
+
+// Func allows the builder to be treated as a Middleware interface.
+func (middleware *MiddlewareBuilder[T]) Func() MiddlewareFunc {
+	return func(ctx *MiddlewareContext) error {
+		err := middleware.Handler(ctx)
+
+		if err != nil && middleware.OnErrorHandler != nil {
+			middleware.OnErrorHandler(ctx, err)
+
+			return nil
+		}
+
+		return err
+	}
+}
+
+// WithInit is a script that the middleware's creator can tune to make it run once after the middleware is hooked
+func (middleware *MiddlewareBuilder[T]) WithInit(initFn func(*T)) *MiddlewareBuilder[T] {
+	initFn(middleware.Config)
+	return middleware
+}
+
+func (middleware *MiddlewareBuilder[T]) OnError(handler func(ctx *MiddlewareContext, err error)) *MiddlewareBuilder[T] {
+	middleware.OnErrorHandler = handler
+	return middleware
+}
+
+// NewMiddlewareBuilder constructs a typed middleware with a config and handler function.
+func NewMiddlewareBuilder[T any](
+	name string,
+	defaultConfig *T,
+	handler func(ctx *MiddlewareContext, config *T) error,
+) *MiddlewareBuilder[T] {
+	builder := &MiddlewareBuilder[T]{
+		Config: defaultConfig,
+		Handler: func(ctx *MiddlewareContext) error {
+			return handler(ctx, defaultConfig)
+		},
+		OnErrorHandler: func(ctx *MiddlewareContext, err error) {
+			logger.Error("Middleware '%s' error: %v\n", name, err)
+		},
+	}
+	return builder
+}
+
+func ConvertMiddewaresToFuncs(mw []Middleware) []MiddlewareFunc {
+	funcs := make([]MiddlewareFunc, len(mw))
+	for i, m := range mw {
+		funcs[i] = m.Func()
+	}
+	return funcs
+}
